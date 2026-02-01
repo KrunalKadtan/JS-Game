cb21532bce0e8bbed64bdb7734a593462963a7ff
Krunal <krunalkadtan19@gmail.com>
2026-02-01 08:29:21 +0000

    Snake Movement Control

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const rows = Math.floor(board.clientHeight / blockHeight);
 const blocks = [];
 
 const snake = [
-  { x: 1, y: 3 },
-  { x: 1, y: 4 },
-  { x: 1, y: 5 }
+  { x: 1, y: 5 },
 ];
+
+let direction = 'right';
 
 for (let row = 0; row < rows; row++) {
   for (let col = 0; col < cols; col++) {
@@ -28,4 +28,52 @@ function render() {
   })
 }
 
-render();
+let start = setInterval(() => {
+  let head = null;
+
+  if (direction === 'right') {
+    head = {
+      x: snake[0].x,
+      y: snake[0].y + 1,
+    };
+  } else if (direction === 'left') {
+    head = {
+      x: snake[0].x,
+      y: snake[0].y - 1,
+    };
+  } else if (direction === 'up') {
+    head = {
+      x: snake[0].x - 1,
+      y: snake[0].y,
+    };
+  } else if (direction === 'down') {
+    head = {
+      x: snake[0].x + 1,
+      y: snake[0].y,
+    };
+  }
+  
+  snake.forEach(segment => {
+    blocks[`${segment.x},${segment.y}`].classList.remove('fill');
+  })
+  
+  snake.unshift(head);
+  snake.pop()
+  
+  render();
+}, 500);
+
+
+addEventListener("keydown", event => {
+  if (event.key === 'ArrowUp') {
+    direction = 'up'
+  } else if (event.key === 'ArrowDown') {
+    direction = 'down'
+  }
+  if (event.key === 'ArrowLeft') {
+    direction = 'left'
+  }
+  if (event.key === 'ArrowRight') {
+    direction = 'right'
+  }
+})
